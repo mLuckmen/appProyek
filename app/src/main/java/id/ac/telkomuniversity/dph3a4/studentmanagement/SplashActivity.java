800c314ec15dbcd6167968c3974e0d66bfff9913
mLuckmen <emluqman 101113@gmail.com>
2019-12-06 12:47:44 +0700

    initial commit

---
+++ app/src/main/java/id/ac/telkomuniversity/dph3a4/studentmanagement/SplashActivity.java
@@ -0,0 +1,26 @@
+package id.ac.telkomuniversity.dph3a4.studentmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+    }
+}
