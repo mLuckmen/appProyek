a381b10bccfdc3a3b965fc1b1e12c38b9d7c1d38
mLuckmen <emluqman 101113@gmail.com>
2019-12-09 14:10:48 +0700

    Sampe Layout sukses

--- app/src/main/java/id/ac/telkomuniversity/dph3a4/studentmanagement/RegisterTwoActivity.java
+++ app/src/main/java/id/ac/telkomuniversity/dph3a4/studentmanagement/RegisterTwoActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class RegisterTwoActivity extends AppCompatActivity {
 
+    Button btn_continue;
     LinearLayout btn_back;
 
     @Override
@@ -22,6 +24,15 @@ public class RegisterTwoActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent backtorev = new Intent(RegisterTwoActivity.this, RegisterOneActivity.class);
                 startActivity(backtorev);
+            }
+        });
+
+        btn_continue = findViewById(R.id.btn_continue);
+        btn_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotosuccess = new Intent(RegisterTwoActivity.this, SuccessRegisterActivity.class);
+                startActivity(gotosuccess);
             }
         });
     }
---
+++ app/src/main/java/id/ac/telkomuniversity/dph3a4/studentmanagement/SuccessRegisterActivity.java
@@ -0,0 +1,14 @@
+package id.ac.telkomuniversity.dph3a4.studentmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SuccessRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_register);
+    }
+}
